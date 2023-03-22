7bacc9eea4931c7c7829b232e965f87501eefbc2
ahmed hossam <ahmedhossamfekry9@gmail.com>
2023-03-21 17:48:43 -0700

    2nd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Alphabet function
+ * is wriiten by ahmed hossam
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet(void);
+	return (0);
+}
+
+void print_alphabet(void);
+{
+	int i = 97;
+	for(i = 97; i<123; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
