82217637ba7a56a59e4a407d545fc33a54748163
ahmed hossam <ahmedhossamfekry9@gmail.com>
2023-04-13 14:26:22 -0700

    finish

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+		
+/**
+ * *malloc_checked - assign to location memory using malloc
+ * @b: num of bytes to allocate		
+ *
+ * Return: a pointer to the memory
+ */
+		
+void *malloc_checked(unsigned int b)
+{
+		
+	void *ptr;
+		
+	ptr = malloc(b);
+		
+	if (ptr == NULL)
+		exit(98);
+				
+	return (ptr);
+		
+}
